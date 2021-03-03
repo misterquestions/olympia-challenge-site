042989733cae1b9482edc5a92ccb01838fc2dc30
MisterQuestions <misterquestions.mx@gmail.com>
2021-03-03 01:34:32 -0600

    feat: add firebase initialization in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import firebase from 'firebase/app';
 import { ThemeProvider } from '@material-ui/core/styles';
 import { CssBaseline } from '@material-ui/core';
 import { BrowserRouter } from 'react-router-dom';
 import LightTheme from './themes/LightTheme';
 import './styles/main.scss';
+
+firebase.initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+});
 
 const App: React.FC = () => {
   return (
