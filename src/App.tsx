2b76dbf440f5feb7c66e9206fddad1beb60f4dd4
MisterQuestions <misterquestions.mx@gmail.com>
2021-03-03 14:44:55 -0600

    fix: remove plugins from main app

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,25 @@
-import React from 'react';
-import firebase from 'firebase/app';
+import React, { Suspense } from 'react';
 import { ThemeProvider } from '@material-ui/core/styles';
 import { CssBaseline } from '@material-ui/core';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import LightTheme from './themes/LightTheme';
+import IndexPage from './pages/IndexPage';
 import './styles/main.scss';
-
-firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-});
 
 const App: React.FC = () => {
   return (
-    <ThemeProvider theme={LightTheme}>
-      <CssBaseline />
-      <BrowserRouter>
-        {/* Todo: Add navigation and routing features */}
-      </BrowserRouter>
-    </ThemeProvider>
+    <Suspense fallback="loading">
+      <ThemeProvider theme={LightTheme}>
+        <CssBaseline />
+        {/* Todo: Add navigation with links of react router */}
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" exact component={IndexPage} />
+          </Switch>
+        </BrowserRouter>
+        {/* Todo: Add footer section */}
+      </ThemeProvider>
+    </Suspense>
   );
 };
 
