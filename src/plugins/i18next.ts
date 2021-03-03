721224275eb497841a4183d0e8a3d0c16e07e21a
MisterQuestions <misterquestions.mx@gmail.com>
2021-03-03 14:45:55 -0600

    feat: add translation plugin

---
+++ src/plugins/i18next.ts
@@ -0,0 +1,18 @@
+import i18next from 'i18next';
+import HttpBackend from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import { initReactI18next } from 'react-i18next';
+
+i18next
+  .use(HttpBackend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: 'en',
+    debug: process.env.NODE_ENV !== 'production',
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18next;
