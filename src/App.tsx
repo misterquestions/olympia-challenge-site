13df8b07bc041d7a650f73d9a1987cd76680cf72
MisterQuestions <misterquestions.mx@gmail.com>
2021-03-03 01:24:16 -0600

    feat: add basic routing and css theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
-import './App.scss';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { CssBaseline } from '@material-ui/core';
+import { BrowserRouter } from 'react-router-dom';
+import LightTheme from './themes/LightTheme';
+import './styles/main.scss';
 
 const App: React.FC = () => {
-  return <p>Hello world I'm a react application</p>;
+  return (
+    <ThemeProvider theme={LightTheme}>
+      <CssBaseline />
+      <BrowserRouter>
+        {/* Todo: Add navigation and routing features */}
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 };
 
 export default App;
