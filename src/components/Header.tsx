707c6c4f5c2793bc1be08a8bff7749ed50f7e01e
root <root@Lakshmi.localdomain>
2021-03-04 19:23:04 -0300

    fix: fixing header styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import { useTranslation } from 'react-i18next';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
@@ -10,21 +11,20 @@ const useStyles = makeStyles((theme) => ({
   root: {
     width: '100%',
   },
-  paper: {
-    padding: theme.spacing(2),
-  },
   space: {
-    marginLeft: 30,
+    marginBottom: 20,
+    justifyContent: 'space-around',
+  },
+  textBold: {
+    fontWeight: 'bold',
   },
   oval: {
     display: 'flex',
-    alignItems: 'center',
     justifyContent: 'center',
-    marginRight: 50,
     backgroundColor: '#D8D8D8',
     borderRadius: '100%',
-    width: 200,
-    height: 200,
+    width: 300,
+    height: 300,
   },
   image: {
     width: '70%',
@@ -38,40 +38,55 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const Header: React.FC = () => {
+  const { t } = useTranslation();
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
-      <Paper className={classes.paper}>
-        <Grid container direction="row" className={classes.space}>
-          <Grid item sm container>
-            <Grid
-              item
-              xs
-              container
-              direction="row"
-              spacing={2}
-              alignContent="center"
-            >
-              <Grid item xs>
-                <Typography gutterBottom variant="h3">
-                  Optimiza tus días
-                </Typography>
-                <Typography variant="subtitle1" gutterBottom>
-                  El poder de elegir Electrodomésticos
-                </Typography>
-                <Typography variant="subtitle1" gutterBottom>
-                  Sustentables, eficientes y a precios accesibles
-                </Typography>
-                <Grid item>
-                  <Button href="/pay-form" variant="contained">
-                    Comprá Ahora
+      <Paper>
+        <Grid container direction="row" justify="space-around">
+          <Grid item xs={6} container>
+            <Grid item container>
+              <Grid item container className={classes.space}>
+                <Grid container direction="column" justify="center">
+                  <Typography
+                    align="center"
+                    gutterBottom
+                    variant="h3"
+                    className={classes.textBold}
+                  >
+                    {t('header.title')}
+                  </Typography>
+                  <Typography
+                    align="center"
+                    variant="subtitle1"
+                    gutterBottom
+                    className={classes.textBold}
+                  >
+                    {t('header.subtitle')}
+                  </Typography>
+                  <Typography
+                    align="center"
+                    variant="subtitle1"
+                    gutterBottom
+                    className={classes.textBold}
+                  >
+                    {t('header.subtitleNext')}
+                  </Typography>
+                </Grid>
+                <Grid item xs={6}>
+                  <Button
+                    href="/pay-form"
+                    variant="contained"
+                    style={{ backgroundColor: 'black', color: 'white' }}
+                  >
+                    {t('header.buyBtn')}
                   </Button>
                 </Grid>
               </Grid>
             </Grid>
           </Grid>
-          <Grid item className={classes.oval}>
+          <Grid item container justify="flex-end" className={classes.oval}>
             <div className={classes.image}>
               <img className={classes.img} alt="complex" src={headerImage} />
             </div>
